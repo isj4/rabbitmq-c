31a84119529e187b6ef035cc8cf9f7294b24ad0a
Ivan Skytte Jørgensen <isj-nedit-dev@i1.dk>
2015-10-17 22:47:48 +0200

    Change char* members of amqp_connection_info to const char* so you can assign them values from string literals without compiler warnings

--- librabbitmq/amqp.h
+++ librabbitmq/amqp.h
@@ -2267,10 +2267,10 @@ AMQP_CALL amqp_destroy_envelope(amqp_envelope_t *envelope);
  * \since v0.2
  */
 struct amqp_connection_info {
-  char *user;                 /**< the username to authenticate with the broker, default on most broker is 'guest' */
-  char *password;             /**< the password to authenticate with the broker, default on most brokers is 'guest' */
-  char *host;                 /**< the hostname of the broker */
-  char *vhost;                /**< the virtual host on the broker to connect to, a good default is "/" */
+  const char *user;           /**< the username to authenticate with the broker, default on most broker is 'guest' */
+  const char *password;       /**< the password to authenticate with the broker, default on most brokers is 'guest' */
+  const char *host;           /**< the hostname of the broker */
+  const char *vhost;          /**< the virtual host on the broker to connect to, a good default is "/" */
   int port;                   /**< the port that the broker is listening on, default on most brokers is 5672 */
   amqp_boolean_t ssl;
 };
